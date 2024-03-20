2e76fb038f1a8889d7abeb9176978679457ba751
YagSill <>
2024-03-20 13:55:41 +0900

    dw

--- www/testSwift.js
+++ www/testSwift.js
@@ -1,9 +1,3 @@
-// var exec = require('cordova/exec');
-
-// exports.coolMethod = function (arg0, success, error) {
-//     exec(success, error, 'testSwift', 'coolMethod', [arg0]);
-// };
-
 window.echo = function(str, callback) {
     cordova.exec(callback, function(err) {
         callback('Nothing to echo.');
